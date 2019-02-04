53ee65e105557fe505fbe3a53417958c9a8e95d2
Paul Bradley <bradleypauld@gmail.com>
2019-02-04 13:26:00 -0600

    Change project structure

--- personalSite.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => res.send('Currently under construction :).'));
-
-app.listen(PORT, () => console.log(`Node.js app listening on port ${port}.`));
